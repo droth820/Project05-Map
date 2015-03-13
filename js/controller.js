7c191785d92f48aa4f7fb8d2a81720de5e334d85
David <davidroth0820@gmail.com>
2015-03-13 13:17:45 -0700

    Initial commit

---
+++ js/controller.js
@@ -0,0 +1,134 @@
+function viewModel() {
+	var self = this;
+	this.map = ko.observable();
+	this.marker = ko.observable();
+	this.lat = "";
+    this.lng = "";
+	this.appendeddatahtml = "";
+	this.arguments = "";
+	this.str = "";
+	this.newstr = "";
+	this.phone = "";
+	this.rating = "";
+	this.address = "";
+	this.searchVenue = ko.observable();
+
+	//Search for venue based on user query
+	function searchVenue(){
+		$("#query").click(function(){
+			$(this).val("");
+		});
+
+		$("#query").blur(function(){
+			if ($(this).val() == "") {
+				$(this).val("Example: McDonalds");
+			}
+		
+			if ($(this).val() != "Example: McDonalds") {
+				$(this).addClass("focus");
+			} else {
+				$(this).removeClass("focus");
+			}
+		});
+
+		//Submit search query and call to getVenues
+		$("#searchform").submit(function(event){
+			event.preventDefault();
+			if (!lat) {
+				navigator.geolocation.getCurrentPosition(getLocation);
+			} else {
+				getVenues();
+			}		
+		});
+
+	}
+	searchVenue();
+
+	
+	//Sets user location for gathering venue information based on search query
+	function getLocation(location) {
+	    lat = location.coords.latitude;
+	    lng = location.coords.longitude;
+		getVenues();
+	}
+	//Connect with Foursquare database and set parameters for displaying retrieved information
+	function getVenues() {
+	$.ajax({
+	  	type: "GET",
+	  	url: "https://api.foursquare.com/v2/venues/explore?ll="+lat+","+lng+"&client_id=AZWA4SWMCZ4T4E3RHWVLUR1FH1I3A3CRXDKWVVQG3HKJHQY5&client_secret=2IK13QP35FXW1VZOX0AGUR1PARR044TKYHPFCULKTGABTZRR&v=20130619&query="+$("#query").val()+"",
+	  	success: function(data) {
+			$("#venues").show();
+			var dataobj = data.response.groups[0].items;
+			$("#venues").html("");
+				
+			// Rebuild the map using data.
+			var myOptions = {
+				zoom:11,
+				center: new google.maps.LatLng(33.306349, -111.839305),
+				mapTypeId: google.maps.MapTypeId.HYBRID,
+				panControl: true,
+				zoomControl: true
+			},
+			map = new google.maps.Map(document.getElementById('map'), myOptions);
+				
+			// Build markers and elements for venues returned.
+			$.each( dataobj, function() {	
+				if (this.venue.contact.formattedPhone) {
+					phone = "Phone:"+this.venue.contact.formattedPhone;
+				} else {
+					phone = "";
+				}
+					
+				if (this.venue.location.address) {
+					address = '<p class="subinfo">'+this.venue.location.address+'<br>';
+				} else {
+					address = "";
+				}
+					
+				if (this.venue.rating) {
+					rating = '<span class="rating">'+this.venue.rating+'</span>';
+				}
+					
+				appendeddatahtml = '<div class="venue"><h2><span>'+this.venue.name+rating+'</span></h2>'+address+phone+'</p><p><strong>Total Checkins:</strong> '+this.venue.stats.checkinsCount+'</p></div>';
+				$("#venues").append(appendeddatahtml);
+					
+				// Build markers
+				var markerImage = {
+				url: 'images/pin2.png',
+				scaledSize: new google.maps.Size(24, 24),
+				origin: new google.maps.Point(0,0),
+				anchor: new google.maps.Point(24/2, 24)
+				},
+				markerOptions = {
+				map: map,
+				position: new google.maps.LatLng(this.venue.location.lat, this.venue.location.lng),
+				title: this.venue.name,
+				animation: google.maps.Animation.DROP,
+				icon: markerImage,
+				optimized: false
+				},
+				marker = new google.maps.Marker(markerOptions)
+			});
+		}
+	});
+}
+
+	
+	//Rebuild map to display markers retrieved from FS database
+	function mapbuild() {
+		$("#venues").hide();
+		var myOptions = {
+		zoom:11,
+		center: new google.maps.LatLng(33.306349, -111.839305),
+		mapTypeId: google.maps.MapTypeId.Hybrid,
+		panControl: true,
+		zoomControl: true,
+		tilt: 45 //Allow user to pan at 45 degree angle when in street view.
+		},
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+	}
+	
+	//Build the map and get things going
+	mapbuild();
+}
+ko.applyBindings(viewModel);
