de6ce5011cb4b3e4d4d84290b05db5ba37128979
David <davidroth0820@gmail.com>
2015-04-06 17:31:41 -0700

    testing knockout

--- js/controll01.js
+++ js/controll01.js
@@ -127,68 +127,6 @@
 				},
 				marker = new google.maps.Marker(markerOptions)
 
-
-		/*function getVenues() {
-	$.ajax({
-	  	type: "GET",
-	  	url: "https://api.foursquare.com/v2/venues/explore?ll="+lat+","+lng+"&client_id=HFOT1XUCTPSBFCWA0W5OMCOLVPWLUA5T0ELRWKDOKAEVRB3V&client_secret=SJRCFDAGLCACDPY1EIEHNITKJIKNCN5KFPOINR0RCPYX35LZ&v=20130619&query="+$("#query").val()+"",
-	  	success: function(data) {
-			$("#venues").show();
-			var dataobj = data.response.groups[0].items;
-			$("#venues").html("");
-				
-			// Rebuild the map using data.
-			var myOptions = {
-				zoom:11,
-				center: new google.maps.LatLng(33.340053, -111.859627),
-				mapTypeId: google.maps.MapTypeId.HYBRID,
-				panControl: true,
-				zoomControl: true
-			},
-			map = new google.maps.Map(document.getElementById('map'), myOptions);
-				
-			// Build markers and elements for venues returned.
-			$.each( dataobj, function() {	
-				if (this.venue.contact.formattedPhone) {
-					phone = "Phone:"+this.venue.contact.formattedPhone;
-				} else {
-					phone = "";
-				}
-					
-				if (this.venue.location.address) {
-					address = '<p class="subinfo">'+this.venue.location.address+'<br>';
-				} else {
-					address = "";
-				}
-					
-				if (this.venue.rating) {
-					rating = '<span class="rating">'+this.venue.rating+'</span>';
-				}
-					
-				appendeddatahtml = '<div class="venue"><p><b><span>'+this.venue.name+'</b>'+rating+'</span></h2>'+address+phone+'</p><p><strong>Total Checkins:</strong> '+this.venue.stats.checkinsCount+'</p></div>';
-				$("#venues").append(appendeddatahtml);
-					
-				// Build markers
-				var markerImage = {
-				url: 'images/ScopePin.png',
-				scaledSize: new google.maps.Size(24, 24),
-				origin: new google.maps.Point(0,0),
-				anchor: new google.maps.Point(24/2, 24)
-				},
-				markerOptions = {
-				map: map,
-				position: new google.maps.LatLng(this.venue.location.lat, this.venue.location.lng),
-				title: this.venue.name,
-				animation: google.maps.Animation.DROP,
-				icon: markerImage,
-				optimized: false
-				},
-				marker = new google.maps.Marker(markerOptions)
-				});
-			}
-		});
-*/
-	}
 ko.applyBindings(viewModel);
 	
 
