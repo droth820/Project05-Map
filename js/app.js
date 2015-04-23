557fdcc20455cc7ffa7a02bd439d4a7df05c7d7e
David <davidroth0820@gmail.com>
2015-04-23 08:19:05 -0700

    working on knockout

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function viewModel() {
 	//Create google map and attach it to div #map
 	var map = new google.maps.Map(document.getElementById('map'), {
      	zoom: 5,
-     	center: new google.maps.LatLng(getLocation()),
+     	center: new google.maps.LatLng(location),
      	mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
