e8cee651d50495d8e8838b09d41a52b016957116
David <davidroth0820@gmail.com>
2015-04-30 14:55:20 -0700

    working on initial map rendering

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
 /*Creates map that will be viewed on website*/
-var locations = [
+/*var locations = [
   ['Valle Luna', 33.319197, -111.874096,'Mexican food restaurant', 'http://www.valleluna.com/'],
   ['Zappone\'s Italian Bistro', 33.380328, -111.724445, 'Italian food restaurant','http://www.zapponesbistro.com/'],
   ['Harkins Towne Center', 33.278889, -111.790663, 'Movie theater','http://www.harkinstheatres.com/']
 
 ];
-
+*/
 //Create google map and attach it to div #map
 var map = new google.maps.Map(document.getElementById('map'), {
      			 zoom: 11,
@@ -13,7 +13,7 @@ var map = new google.maps.Map(document.getElementById('map'), {
      			 mapTypeId: google.maps.MapTypeId.HYBRID
     			});
 
-   			 var infowindow = new google.maps.InfoWindow();
+   			/* var infowindow = new google.maps.InfoWindow();
 
           
     			var marker, i;
@@ -31,3 +31,4 @@ var map = new google.maps.Map(document.getElementById('map'), {
         			}
       			})(marker, i));
    			 }
+*/
