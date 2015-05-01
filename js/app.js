7b0a20cee3b8e2e3de516450fa7446aa74f3c8a1
David <davidroth0820@gmail.com>
2015-04-30 19:15:00 -0700

    working on initial map build

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
+var locations = [
+  ['Valle Luna', 33.319197, -111.874096,'Mexican food restaurant'],
+  ['Zappone\'s Italian Bistro', '33.380328, -111.724445', 'Italian food restaurant'],
+  ['Harkins Towne Center', '33.278889, -111.790663', 'Movie theater']
+
+];
+
 function initialize() {
 	var mapOptions = {
     center: new google.maps.LatLng(33.340053, -111.859627),
     zoom: 11,
     mapTypeId: google.maps.MapTypeId.Hybrid
   };
+  	//add infowindow
+  	var infowindow = new google.maps.InfoWindow();
+  	//iterate through locations and display markers
+  	var marker, i;
+  	for (i = 0; i < locations.length; i++) {
+  		marker = new google.maps.Marker({
+  			icon: 'images/pin2.png',
+  			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+  			map: map
+  		});
+
+  		//add infowindow
+  		google.maps.event.addListener(marker, 'click', (function(marker, i)) {
+  			return function() {
+  				infowindow.setContent('<div><h2>'+ locations[i][0]+ '</h2></div>' +'<div>'+locations [i][2]+'</div>');
+         		infowindow.open(map, marker);
+  			}
+  		}(marker, i));
+  	}
+
 
 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
