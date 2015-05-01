2ee92476f08153e2a2296aa48ca8f2a3b26af534
David <davidroth0820@gmail.com>
2015-05-01 10:26:05 -0700

    separating controller for initial map build

--- js/map.js
+++ js/map.js
@@ -1,34 +1,12 @@
-/*Creates map that will be viewed on website*/
-/*var locations = [
-  ['Valle Luna', 33.319197, -111.874096,'Mexican food restaurant', 'http://www.valleluna.com/'],
-  ['Zappone\'s Italian Bistro', 33.380328, -111.724445, 'Italian food restaurant','http://www.zapponesbistro.com/'],
-  ['Harkins Towne Center', 33.278889, -111.790663, 'Movie theater','http://www.harkinstheatres.com/']
+function initialize() {
+	var mapOptions = {
+    center: new google.maps.LatLng(33.340053, -111.859627),
+    zoom: 11,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
 
-];
-*/
-//Create google map and attach it to div #map
-var map = new google.maps.Map(document.getElementById('map'), {
-     			 zoom: 11,
-     			 center: new google.maps.LatLng(33.340053, -111.859627),
-     			 mapTypeId: google.maps.MapTypeId.HYBRID
-    			});
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
 
-   			/* var infowindow = new google.maps.InfoWindow();
-
-          
-    			var marker, i;
-    			for (i = 0; i < locations.length; i++) {  
-     			 marker = new google.maps.Marker({
-              icon: 'images/pin2.png',
-      			  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-     			   map: map
-     			 });
-
-     			 google.maps.event.addListener(marker, 'click', (function(marker, i) {
-       			 return function() {
-         			 infowindow.setContent('<div><h2>'+ locations[i][0]+'</h2></div>'+'<div>'+locations[i][3]+'</div><br>'+'<div><a href="'+locations[i][4]+'"></div>');
-         			 infowindow.open(map, marker);
-        			}
-      			})(marker, i));
-   			 }
-*/
+google.maps.event.addDomListener(window, 'load', initialize);
+ 
